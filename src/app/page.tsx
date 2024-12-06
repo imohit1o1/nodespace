97d51cd7fca9407d9ebe3c7bd4399f848b2a3650
imohit1o1 <mohitk1595@gmail.com>
2024-12-06 23:25:54 +0530

    added next js auth config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ThemeModeToggle } from "@/components/theme-toggle";
 export default function Home() {
   return (
     <div className="flex justify-between items-center max-w-7xl mx-auto p-4">
-      <h1 className="text-xl">SyncFusion</h1>
+      <h1 className="text-xl">NodeSpace</h1>
       <div className="flex gap-4">
         <ThemeModeToggle />
         <Button className="">Login</Button>

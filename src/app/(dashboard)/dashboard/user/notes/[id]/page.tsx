da6961ae77343aa49c3e5a89bd142d83a7e6af2e
imohit1o1 <mohitk1595@gmail.com>
2024-12-10 21:21:26 +0530

    Add folder structure for notes feature with dynamic folder and subfolder handling

---
+++ src/app/(dashboard)/dashboard/user/notes/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NoteById() {
+  return <div>NoteById</div>;
+}
---
+++ src/app/(dashboard)/dashboard/user/notes/folder/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotesFolderById() {
+  return <div>NotesFolderById</div>;
+}
